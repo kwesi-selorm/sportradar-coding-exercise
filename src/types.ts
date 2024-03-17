96bd5cdb7ea922db1e2d28f15b10ae3e72aa5d89
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2024-03-17 10:01:39 +0100

    Create types file

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+interface IMatch {
+  id: string;
+  homeTeam: string;
+  awayTeam: string;
+  homeScore: number;
+  awayScore: number;
+  date: string;
+  startTime: string;
+}
+
+export { type IMatch };
