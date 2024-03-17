79a8241c0d7a9f2b2bbc0a92aa5e7066c6a92dec
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2024-03-17 12:41:58 +0100

    Add a new match to show sorting of matches with the same total score

--- src/data/matches.ts
+++ src/data/matches.ts
@@ -1,7 +1,8 @@
-import Match from '../classes/Match.ts'
+import Match from "../classes/Match";
 
-const match1 = new Match('Germany', 'Poland', 3, 1, '2023-11-04', '10:00')
-const match2 = new Match('France', 'Belgium', 3, 2, '2023-10-18', ' 16:00')
-const match3 = new Match('Croatia', 'Senegal', 2, 0, '2023-12-12', ' 18:00')
+const match1 = new Match("Germany", "Poland", 3, 1, "2023-11-04", "10:00");
+const match2 = new Match("Ghana", "Australia", 4, 1, "2023-10-19", "10:00");
+const match3 = new Match("France", "Belgium", 3, 2, "2023-10-18", " 16:00");
+const match4 = new Match("Croatia", "Senegal", 2, 0, "2023-12-12", " 18:00");
 
-export default [match1, match2, match3]
+export default [match1, match2, match3, match4];
