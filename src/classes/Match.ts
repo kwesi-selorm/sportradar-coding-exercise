2953260e4f9ceb3e3e7429e8aa30043279069aa9
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2024-03-17 12:52:16 +0100

    Prettier formatting

--- src/classes/Match.ts
+++ src/classes/Match.ts
@@ -1,36 +1,36 @@
-import { IMatch } from "../types";
+import { IMatch } from '../types'
 
 class Match implements IMatch {
-  id: string;
-  homeTeam: string;
-  awayTeam: string;
-  homeScore: number;
-  awayScore: number;
-  date: string;
-  startTime: string;
+  id: string
+  homeTeam: string
+  awayTeam: string
+  homeScore: number
+  awayScore: number
+  date: string
+  startTime: string
   constructor(
     homeTeam: string,
     awayTeam: string,
     homeScore: number,
     awayScore: number,
     date: string,
-    startTime: string,
+    startTime: string
   ) {
     this.id = `${homeTeam.substring(0, 3).toUpperCase()}_${awayTeam
       .substring(0, 3)
-      .toUpperCase()}`;
-    this.homeTeam = homeTeam;
-    this.awayTeam = awayTeam;
-    this.homeScore = homeScore;
-    this.awayScore = awayScore;
-    this.date = date;
-    this.startTime = startTime;
+      .toUpperCase()}`
+    this.homeTeam = homeTeam
+    this.awayTeam = awayTeam
+    this.homeScore = homeScore
+    this.awayScore = awayScore
+    this.date = date
+    this.startTime = startTime
   }
 
   updateScore(home: number, away: number) {
-    this.homeScore = home;
-    this.awayScore = away;
+    this.homeScore = home
+    this.awayScore = away
   }
 }
 
-export default Match;
+export default Match
