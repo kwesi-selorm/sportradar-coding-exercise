d0262ed9b27a3f284e4a4b5f3603d23f9cee372f
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2024-03-17 10:04:22 +0100

    Matches list for scoreboard initialisation

---
+++ src/data/matches.ts
@@ -0,0 +1,7 @@
+import Match from '../classes/Match.ts'
+
+const match1 = new Match('Germany', 'Poland', 3, 1, '2023-11-04', '10:00')
+const match2 = new Match('France', 'Belgium', 3, 2, '2023-10-18', ' 16:00')
+const match3 = new Match('Croatia', 'Senegal', 2, 0, '2023-12-12', ' 18:00')
+
+export default [match1, match2, match3]
